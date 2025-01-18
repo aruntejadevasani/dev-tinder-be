9babb43f7a7f56f9cece24a92dbebaa232a968ae
Arun Teja <aruntejadevasani@gmail.com>
2025-01-18 20:34:15 +0530

    Added validations to Schema

--- src/app.js
+++ src/app.js
@@ -1,33 +1,84 @@
-const express = require('express');
-const connectDB = require('./config/database');
-const UserModel = require('./models/user');
+const express = require("express");
+const connectDB = require("./config/database");
+const UserModel = require("./models/user");
 const app = express();
 
-app.post('/signup', async (req, res) => {
-    // Creating new instance of the UserModel.
-    const user = new UserModel({
-        firstName: 'Teja',
-        lastName: 'Deva',
-        email: 'teja@deva.com',
-        password: 'teja@123'
-    });
+app.use(express.json());
 
-    try {
-        await user.save();
-        res.send('User Added!')
-    } catch (err) {
-        res.status(400).send('Error occured when saving data: ' + err.message);
+// SignUp
+app.post("/signup", async (req, res) => {
+  // Creating new instance of the UserModel.
+
+  try {
+    const user = new UserModel(req.body);
+    await user.save();
+    res.send("User Added!");
+  } catch (err) {
+    res.status(400).send("Error occured when saving data: " + err.message);
+  }
+});
+
+// Get user by email ID
+app.get("/user", async (req, res) => {
+  try {
+    const users = await UserModel.find({ firstName: req.body.firstName });
+    if (users.length === 0) {
+      res.status(404).send("User not found!");
+    } else {
+      res.send(users);
     }
+  } catch (err) {
+    res.status(400).send("Something Went Wrong...");
+  }
+});
 
-})
+// Get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something Went Wrong...");
+  }
+});
+
+// Delete a user
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await UserModel.findByIdAndDelete(userId);
+    console.log(user);
+    if (!user) {
+      res.status(404).send("User not found..!");
+    } else {
+      res.send("User Deleted!");
+    }
+  } catch (err) {
+    res.status(400).send("Something Went Wrong...");
+  }
+});
+
+// Update data of a User
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await UserModel.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+    });
+    res.send("User Updated!");
+  } catch (err) {
+    res.status(400).send("Something Went Wrong..." + err.message);
+  }
+});
 
 connectDB()
-.then(()=>{
-    console.log('DB Connection Succesfull...');
-    app.listen(3000, ()=> {
-        console.log('Server listening to port 3000...');
+  .then(() => {
+    console.log("DB Connection Succesfull...");
+    app.listen(3000, () => {
+      console.log("Server listening to port 3000...");
     });
-})
-.catch(()=>{
-    console.log('DB Connection Error!');
-})
+  })
+  .catch(() => {
+    console.log("DB Connection Error!");
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    firstName: {type: String},
-    lastName: {type: String},
-    emailId: {type: String},
-    password: {type: String},
-    age: {type: Number},
-    gender: {type: String}
-});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid!");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.pnrao.com/wp-content/uploads/2023/06/dummy-user-male.jpg",
+    },
+    about: {
+      type: String,
+      default: "This is the default about the user",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
 
-const UserModel = mongoose.model('User', userSchema);
+const UserModel = mongoose.model("User", userSchema);
 
 module.exports = UserModel;
